e27fccbef2ba83c5ad7e92e9636348ddbad09343
Chandlerzz <Chandlerzz@github.com>
2021-04-06 08:27:04 +0800

    1

--- scrapy/hello/hello.go
+++ scrapy/hello/hello.go
@@ -1,37 +1,4 @@
-<<<<<<< HEAD
 package main
-
-import (
-	"net/http"
-	"reflect"
-	"io/ioutil"
-	"github.com/PuerkitoBio/goquery"
-	"fmt"
-)
-
-func main() {
-	resp, err := http.Get("https://www.dbooks.org/blazor-webassembly-succinctly-164200202x/")
-	if err != nil {
-		fmt.Println(err)
-		fmt.Println(err)
-	}
-	defer resp.Body.Close()
-	doc,err := goquery.NewDocumentFromReader(resp.Body)
-	if err !=nil {
-		fmt.Println(err)
-	}
-	doc.Find(".col100").Each(func(i int, s *goquery.Selection){
-		title := s.Find("H1").Text()
-		fmt.Println(i,title)
-	})
-	body, _ := ioutil.ReadAll(resp.Body)
-	body1 := string(body)
-	fmt.Printf("%s",reflect.TypeOf(body1))
-	fmt.Printf("%s", reflect.TypeOf(body))
-}
-=======
-package main
-
 import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
@@ -74,4 +41,3 @@ func main() {
 		fmt.Println(i,title)
 	})
 }
->>>>>>> bc5c6617030c088395eea10e14bf18ce4e34d7c2
