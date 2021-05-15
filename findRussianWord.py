8e3755204b5da60e36415204092ed4e52a9cd794
Chandlerzz <664837078@qq.com>
2021-05-15 00:23:25 -0400

    check if endwith \n

--- findRussianWord.py
+++ findRussianWord.py
@@ -1,7 +1,23 @@
+# python 默认编码为unicode
+#这里匹配的是俄语字符所有unicode码
 import re
 russian_word = re.compile(u'[\u0411-\u04ff]+')
 with open(r"source/source.txt") as file:
-    for i in range(10):
+    words={}
+    wordkey=[]
+    flag = True
+    while flag:
         line = file.readline()
+        # check if endwith \n 
+        if line.endswith("\n"):
+            flag = True
+        else:
+            flag = False
         wordarr = russian_word.findall(line)
-        print(wordarr)
+        for word in wordarr:
+            if word in wordkey:
+                words[word] = words[word]+1
+            else:
+                words[word] = 1
+                wordkey.append(word)
+               
